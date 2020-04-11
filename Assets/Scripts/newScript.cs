b58ef80a02cdf97a40ce01f182c6560e1302c30f
Sachin Sharma <sachin.sharma@jungleworks.com>
2020-04-11 10:59:41 +0530

    first commit

---
+++ Assets/Scripts/newScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class newScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    GameObject m_instantprefab;
+
+
+    public GameObject placedPrefab{
+    	get{ return m_instantprefab;}
+    	set{ m_instantprefab = value;}
+    }
+
+    public GameObject spawnedObject{
+    	get;
+    	private set;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
